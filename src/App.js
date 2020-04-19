7e9afa2d026bf047ce2bfb7dd454899b1a5f8ab5
mani21na <mani21na@gmail.com>
2020-04-18 21:02:08 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
 
   handleOnClick() {
+    //dispatch is automatically provided by connect 
+    //if it is missing a second argument
     this.props.dispatch({
       type: 'INCREASE_COUNT',
     });
-  }
+  } 
 
   render() {
     return (
@@ -21,4 +24,9 @@ class App extends Component {
   }
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return { itmes: state.item };
+};
+
+//connect: a function that listens to every change in the store
+export default connect(mapStateToProps)(App);
